82a9bdad988c5f59234a0cfe49c55454d319cd27
Logan Thimer <thimerlogan@gmail.com>
2025-01-26 09:32:17 -0700

    Added exceptions

---
+++ StocksApp/StocksApp.Domain/Exceptions/NotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace StocksApp.Domain.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ StocksApp/StocksApp.Domain/Exceptions/UnauthorizedResourceAccessException.cs
@@ -0,0 +1,9 @@
+﻿namespace StocksApp.Domain.Exceptions
+{
+    public class UnauthorizedResourceAccessException : Exception
+    {
+        public UnauthorizedResourceAccessException(string message) : base(message)
+        {
+        }
+    }
+}
