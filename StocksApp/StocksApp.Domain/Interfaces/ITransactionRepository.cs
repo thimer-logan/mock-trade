9b1728fcc7fd79710844c8ddbb9077bbf362d60f
Logan Thimer <thimerlogan@gmail.com>
2025-01-27 06:53:11 -0700

    Added repository code for transactions

---
+++ StocksApp/StocksApp.Domain/Interfaces/ITransactionRepository.cs
@@ -0,0 +1,11 @@
+﻿using StocksApp.Domain.Entities.Stocks;
+
+namespace StocksApp.Domain.Interfaces
+{
+    public interface ITransactionRepository
+    {
+        Task<Transaction?> GetTransactionByIdAsync(Guid id);
+        Task<IEnumerable<Transaction>> GetTransactionsByPortfolioAsync(Guid portfolioId);
+        Task<Transaction> CreateTransactionAsync(Transaction transaction);
+    }
+}
---
+++ StocksApp/StocksApp.Infrastructure/Repositories/TransactionRepository.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StocksApp.Domain.Entities.Stocks;
+using StocksApp.Domain.Interfaces;
+using StocksApp.Infrastructure.DbContext;
+
+namespace StocksApp.Infrastructure.Repositories
+{
+    public class TransactionRepository : ITransactionRepository
+    {
+        private readonly AppDbContext _dbContext;
+
+        public TransactionRepository(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Transaction> CreateTransactionAsync(Transaction transaction)
+        {
+            _dbContext.Transactions.Add(transaction);
+            await _dbContext.SaveChangesAsync();
+            return transaction;
+        }
+
+        public async Task<Transaction?> GetTransactionByIdAsync(Guid id)
+        {
+            return await _dbContext.Transactions.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Transaction>> GetTransactionsByPortfolioAsync(Guid portfolioId)
+        {
+            return await _dbContext.Transactions
+                .Where(t => t.PortfolioId == portfolioId)
+                .ToListAsync();
+        }
+    }
+}
