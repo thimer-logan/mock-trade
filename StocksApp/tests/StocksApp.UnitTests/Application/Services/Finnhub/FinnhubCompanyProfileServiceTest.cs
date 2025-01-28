b75895669a869e1f9e0ef1e9cc6c1949570c0f32
Logan Thimer <thimerlogan@gmail.com>
2025-01-28 06:21:53 -0700

    Changed folder structure

---
+++ StocksApp/tests/StocksApp.UnitTests/Application/Services/Finnhub/FinnhubCompanyProfileServiceTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StocksApp.UnitTests.Application.Services.Finnhub
+{
+    internal class FinnhubCompanyProfileServiceTest
+    {
+    }
+}
